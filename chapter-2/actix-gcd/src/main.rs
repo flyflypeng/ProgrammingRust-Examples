8adbf4afbf1ca226815bc5c6a988b9f3b182831e
flyflypeng <flyflyflypeng@gmail.com>
2021-09-29 13:13:44 +0800

    add post_grd function in the actix_web example
    Signed-off-by: flyflypeng <flyflyflypeng@gmail.com>

--- chapter-2/actix-gcd/src/main.rs
+++ chapter-2/actix-gcd/src/main.rs
@@ -1,7 +1,31 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
 
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_grd))
+    });
 
     println!("Serving on http://localhost:3000...");
     server
@@ -22,4 +46,22 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
     )
+}
+
+fn post_grd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} \
+                                    is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
